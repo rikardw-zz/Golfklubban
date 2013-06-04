a6f282ef045116410c0ae9fe967f80cd7c1b1a5e
Patrick Nyfeldt <patrick.nyfeldt@hotmail.com>
2013-06-04 14:36:16 +0200

    Undersöker om det finns text i teamnamn
    Lagt till en ifsats som kollar så att man skriver in ett namn när man
    skapar ett nytt lag

--- Golfklubban/CompetitionChart.cs
+++ Golfklubban/CompetitionChart.cs
@@ -100,24 +100,28 @@ namespace Golfklubban
         }
         private void btnCreateTeam_Click(object sender, EventArgs e)
         {
-            NpgsqlConnection conn = new NpgsqlConnection("Server=webblabb.miun.se;Port=5432;Database=grp3vt13;User Id=grp3vt13;Password=XmFGFwX6t;SSL=true");
-            try
+            if ((txtTeamName.Text.Trim().Length == 0)) //kod som undersöker om det finns någon text i textbox
+            { MessageBox.Show("Du måste välja ett namn på laget!"); }
+            else
             {
-                conn.Open();
-                NpgsqlCommand command = new NpgsqlCommand("INSERT INTO team (name) VALUES ('" + txtTeamName.Text + "')", conn);
-                int antal = command.ExecuteNonQuery();
+                NpgsqlConnection conn = new NpgsqlConnection("Server=webblabb.miun.se;Port=5432;Database=grp3vt13;User Id=grp3vt13;Password=XmFGFwX6t;SSL=true");
+                try
+                {
+                    conn.Open();
+                    NpgsqlCommand command = new NpgsqlCommand("INSERT INTO team (name) VALUES ('" + txtTeamName.Text + "')", conn);
+                    int antal = command.ExecuteNonQuery();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+                finally
+                {
+                    conn.Close();
+                }
+                lbTeamChart.DataSource = Methods.GetTeams();
             }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.ToString());
-            }
-            finally
-            {
-                conn.Close();
-            }
-            lbTeamChart.DataSource = Methods.GetTeams();
         }
-
         private void lbTeamChart_SelectedIndexChanged(object sender, EventArgs e)
         {
             label12.Text = Convert.ToString(lbTeamChart.SelectedItem.ToString()) + "";
